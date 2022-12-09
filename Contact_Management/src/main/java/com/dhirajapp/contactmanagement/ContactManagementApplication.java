b37e8bf3e42ef2395e3e035d1bb9666d6df9131d
dhirajapp <dhirajkumar31298@gmail.com>
2022-12-09 14:08:24 +0530

    project setup ready

---
+++ Contact_Management/src/main/java/com/dhirajapp/contactmanagement/ContactManagementApplication.java
@@ -0,0 +1,15 @@
+package com.dhirajapp.contactmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactManagementApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Dhiraj");
+		SpringApplication.run(ContactManagementApplication.class, args);
+		System.out.println("project setup has been Ready.");
+	}
+
+}
