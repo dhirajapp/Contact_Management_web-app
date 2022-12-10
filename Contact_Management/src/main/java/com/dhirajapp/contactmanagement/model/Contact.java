9be35e87840527fa34de88b9fcd402f92ef6f5d0
dhirajapp <dhirajkumar31298@gmail.com>
2022-12-10 07:27:48 +0530

    create contact class and contactservice interface

---
+++ Contact_Management/src/main/java/com/dhirajapp/contactmanagement/model/Contact.java
@@ -0,0 +1,10 @@
+package com.dhirajapp.contactmanagement.model;
+
+public class Contact {
+ 
+	private int contactId;
+	private String contactName;
+	private String contactEmail;
+	private long contactNum;
+	
+}
